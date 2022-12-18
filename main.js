8ae2c38101b6704579fe33cbbdcc160d980068fa
Vinícius Oliveira <vinnissuz@hotmail.com>
2022-12-18 12:07:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 // criando o corpo da pagina
 
-function build(){
+function build() {
 	document.body.innerHTML += '<span id="menu"><h1 id="logo">CINECULT</h1><div id="theme"><input type="checkbox" name="ck" id="th"><label for="th" id="lbTh"><span></span></label></div></span><nav id="nv"><span id="nav"><input type="text" id="search" placeholder="Search"><button id="searchBtn"><img src="https://cdn-icons-png.flaticon.com/512/151/151773.png" alt="OK" width="20" height="20"></button></span></nav>';
 
 	// <span id="menuLg"><a href="">Séries</a><a href="">Ao vivo</a></span>
 
-	document.body.innerHTML += '<div id="section1"><div id="m0"></div><div id="m1"></div><div id="m2"></div><div id="m3"></div></div>';
+	document.body.innerHTML += '<div id="section1"><div id="m0"></div><div id="m1"></div><div id="m2"></div><div id="m3"></div><div id="ifrm"><img src="https://cdn-icons-png.flaticon.com/512/3683/3683627.png" width="30" id="back"><iframe frameborder="0" allowfullscreen="true" name="iframe" id="iframe"></div><div id="ifrm"><img src="https://cdn-icons-png.flaticon.com/512/3683/3683627.png" width="30" id="back"><iframe frameborder="0" allowfullscreen="true" name="iframe" id="iframe"></div></div>';
 
 	document.body.innerHTML += '<footer id="ft"><a href="" id="cntt">Contato</a><div id="name"><p>©2022 STRM97, Inc</p><a href="http://github.com/viuh9997" target="_blank">@viuh9997</a></div></footer>';
 
-	document.body.innerHTML += '<div id="ifrm"><img src="https://cdn-icons-png.flaticon.com/512/3683/3683627.png" width="30" id="back"><iframe frameborder="0" allowfullscreen="true" name="iframe" id="iframe"></div>';
-	
 	// theme
 
 	const checkbox = document.getElementById("th");
@@ -75,7 +73,7 @@ function build(){
 
 							document.getElementById(
 								"m0"
-							).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster" onclick="openIframe()"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
+							).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
 						})
 						.catch(function (error) {
 							console.log(error);
@@ -124,7 +122,7 @@ function build(){
 
 						document.getElementById(
 							"m1"
-						).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster" onclick="openIframe()"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
+						).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
 					})
 					.catch(function (error) {
 						console.log(error);
@@ -171,7 +169,7 @@ function build(){
 
 						document.getElementById(
 							"m2"
-						).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster" onclick="openIframe()"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
+						).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
 					})
 					.catch(function (error) {
 						console.log(error);
@@ -218,7 +216,7 @@ function build(){
 
 						document.getElementById(
 							"m3"
-						).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster" onclick="openIframe()"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
+						).innerHTML += `<a href="https://embed.warezcdn.com/filme/${outcome.imdb_id}" target="iframe" id="posters1" class="poster"><img src="https://image.tmdb.org/t/p/w500/${outcome.poster_path}" width="250"></a>`;
 					})
 					.catch(function (error) {
 						console.log(error);
@@ -229,16 +227,28 @@ function build(){
 		.catch(function (error) {
 			console.log(error);
 		});
-
+	
 	// iframe e funções
 	var back = document.getElementById("back");
-	var tl = document.querySelectorAll(".poster");
+	var img0 = document.getElementById("m0");
+	var img1 = document.getElementById("m1");
+	var img2 = document.getElementById("m2");
+	var img3 = document.getElementById("m3");
+
 
 	back.addEventListener("click", function () {
 		document.getElementById("ifrm").style.display = "none";
 	});
-
-	function openIframe() {
+	img0.addEventListener("click", function () {
 		document.getElementById("ifrm").style.display = "flex";
-	}
+	});
+	img1.addEventListener("click", function () {
+		document.getElementById("ifrm").style.display = "flex";
+	});
+	img2.addEventListener("click", function () {
+		document.getElementById("ifrm").style.display = "flex";
+	});
+	img3.addEventListener("click", function () {
+		document.getElementById("ifrm").style.display = "flex";
+	});
 }
